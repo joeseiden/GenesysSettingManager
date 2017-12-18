31247c0cfd2df6750db08ddaa73d884ccca9e89d
Joe Seiden <joe.seiden214@gmail.com>
2017-12-18 10:33:27 -0800

    Create skills migration

---
+++ db/migrate/20171218182830_create_skills.rb
@@ -0,0 +1,13 @@
+class CreateSkills < ActiveRecord::Migration[5.1]
+  def change
+    create_table :skills do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.integer :attribute_id, null: false
+      t.integer :category_id, null: false
+
+      t.timestamps
+    end
+    add_index :name, :attribute_id, :category_id
+  end
+end
